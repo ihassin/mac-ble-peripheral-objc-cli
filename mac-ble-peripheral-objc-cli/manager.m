be73ead595513a47e80789d12847543c388bbdf5
Itamar Hassin <ihassin@mac.com>
2018-01-15 11:56:52 -0500

    Added error checking

--- mac-ble-peripheral-objc-cli/manager.m
+++ mac-ble-peripheral-objc-cli/manager.m
@@ -1,6 +1,5 @@
 //
-//  Manager.m
-//  mac-ble
+//  manager.m
 //
 //  Created by Itamar Hassin on 12/25/17.
 //  Copyright © 2017 Itamar Hassin. All rights reserved.
@@ -14,9 +13,11 @@
 {
     CBPeripheralManager *_peripheralManager;
     Boolean _running;
+    NSString *_peripheralData;
+    CBUUID *_charUUID;
+    CBUUID *_myUUID;
 }
 
-// Constructor
 - (id) init
 {
     self = [super init];
@@ -24,11 +25,15 @@
     {
         _peripheralManager = [[CBPeripheralManager alloc] initWithDelegate:self queue:nil];
         _running = true;
+        _peripheralData = [NSString stringWithFormat:@"ItaBaby"];
+
+        _charUUID = [CBUUID UUIDWithString:@"DDCA9B49-A6F5-462F-A89A-C2144083CA7F"];
+        _myUUID = [CBUUID UUIDWithString:@"BD0F6577-4A38-4D71-AF1B-4E8F57708080"];
     }
     return self;
 }
 
-// Method called whenever the BLE state changes.
+// Method called whenever BT state changes.
 - (void)peripheralManagerDidUpdateState:(CBPeripheralManager *)peripheral
 {
     CBManagerState state = [peripheral state];
@@ -70,55 +75,63 @@
         return;
     }
     
-    CBUUID *myUUID = [CBUUID UUIDWithString:@"BD0F6577-4A38-4D71-AF1B-4E8F57708080"];
     CBMutableService *service;
+    service = [[CBMutableService alloc] initWithType:_myUUID primary:YES];
     
-    [_peripheralManager setDelegate:self];
+    CBMutableCharacteristic *myCharacteristic = [[CBMutableCharacteristic alloc] initWithType:_charUUID properties:CBCharacteristicPropertyRead|CBCharacteristicPropertyIndicate|CBCharacteristicPropertyWriteWithoutResponse value:nil permissions:CBAttributePermissionsReadable|CBAttributePermissionsWriteable];
     
-    service = [[CBMutableService alloc] initWithType:myUUID primary:YES];
-    
-    CBMutableCharacteristic *characteristic = [[CBMutableCharacteristic alloc] initWithType:[CBUUID UUIDWithString:@"DDCA9B49-A6F5-462F-A89A-C2144083CA7F"] properties:CBCharacteristicPropertyRead|CBCharacteristicPropertyIndicate|CBCharacteristicPropertyWriteWithoutResponse value:nil permissions:CBAttributePermissionsReadable];
-    
-    service.characteristics = @[characteristic];
-    
+    service.characteristics = @[myCharacteristic];
     [_peripheralManager addService:service];
-    
+
     [_peripheralManager startAdvertising:@{
                                    CBAdvertisementDataLocalNameKey: @"ITAMAR-MAC-BOOK-PRO",
-                                   CBAdvertisementDataServiceUUIDsKey: @[myUUID]
+                                   CBAdvertisementDataServiceUUIDsKey: @[_myUUID]
                                    }];
 }
 
 - (void)peripheralManagerDidStartAdvertising:(CBPeripheralManager *)peripheral error:(NSError *)error
 {
     NSLog(@"peripheralManagerDidStartAdvertising: %@", peripheral.description);
+    if (error) {
+        NSLog(@"Error advertising: %@", [error localizedDescription]);
+    }
 }
 
 - (void)peripheralManager:(CBPeripheralManager *)peripheral didAddService:(CBService *)service error:(NSError *)error
 {
     NSLog(@"peripheralManagerDidAddService: %@ %@", service, error);
+    if (error) {
+        NSLog(@"Error publishing service: %@", [error localizedDescription]);
+    }
 }
 
 - (void)peripheralManager:(CBPeripheralManager *)peripheral didReceiveReadRequest:(CBATTRequest *)request
 {
-    NSLog(@"didReceiveReadRequest: %@ %@", request.central, request.characteristic);
-    NSString *mainString = [NSString stringWithFormat:@"ItaBaby"];
-    NSData *cmainData= [mainString dataUsingEncoding:NSUTF8StringEncoding];
-    request.value = cmainData;
-    [peripheral respondToRequest:request withResult:CBATTErrorSuccess];
+    if ([request.characteristic.UUID isEqual:_charUUID]) {
+        request.value = [_peripheralData dataUsingEncoding:NSUTF8StringEncoding];
+        [peripheral respondToRequest:request withResult:CBATTErrorSuccess];
+        NSLog(@"didReceiveReadRequest: %@ %@. Returning %@", request.central, request.characteristic.UUID, _peripheralData);
+    } else
+    {
+        NSLog(@"didReceiveReadRequest: %@ %@. Ignoring!", request.central, request.characteristic.UUID);
+    }
 }
 
-- (void)peripheralManager:(CBPeripheralManager *)peripheral
-                  central:(CBCentral *)central
-didSubscribeToCharacteristic:(CBCharacteristic *)characteristic
+- (void)peripheralManager:(CBPeripheralManager *)peripheral didReceiveWriteRequests:(NSArray<CBATTRequest *> *)requests
+{
+    CBATTRequest *request = requests[0];
+    if ([request.characteristic.UUID isEqual:_charUUID]) {
+        _peripheralData =[NSString stringWithUTF8String:[request.value bytes]];
+        NSLog(@"didReceiveWriteRequest: Wrote: %@", _peripheralData);
+    } else
+    {
+        NSLog(@"didReceiveWriteRequest: %@ %@. Ignoring!", request.central, request.characteristic.UUID);
+    }
+}
+
+- (void)peripheralManager:(CBPeripheralManager *)peripheral central:(CBCentral *)central didSubscribeToCharacteristic:(CBCharacteristic *)characteristic
 {
     NSLog(@"didSubscribeToCharacteristic");
-}
-
-- (void)peripheralManager:(CBPeripheralManager *)peripheral
-  didReceiveWriteRequests:(NSArray<CBATTRequest *> *)requests
-{
-    NSLog(@"didReceiveWriteRequests: %@", requests);
 }
 
 - (Boolean) running
